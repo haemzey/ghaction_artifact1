c497805ee40ee56cea4268e24f2de938555da734
Hamza Mughal <muhammadhamzazahid4499@gmail.com>
2026-09-19 17:33:16 +0500

    feat: test/py_test.py

---
+++ test/py_test.py
@@ -0,0 +1,33 @@
+import pytest
+
+from app import app
+
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+
+    with app.test_client() as client:
+        yield client
+
+
+def test_home(client):
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert data["message"] == "Welcome to My Page, Greetings by Hamza"
+    assert data["platform"] == "GitHub Actions"
+    assert data["runtime"] == "Docker + Flask"
+
+
+def test_health(client):
+    response = client.get("/health")
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert data["status"] == "healthy"
