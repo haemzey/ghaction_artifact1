9a49f10f3e16a0909ab16ee6ed6f09bdb7d860fc
Hamza Mughal <muhammadhamzazahid4499@gmail.com>
2026-09-19 17:32:36 +0500

    feat: app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def home():
+    app.logger.info("Homepage endpoint invoked")
+    return jsonify(
+        message="Welcome to My Page, Greetings by Hamza",
+        platform="GitHub Actions",
+        runtime="Docker + Flask",
+    )
+
+
+@app.get("/health")
+def health():
+    app.logger.info("Health check endpoint invoked")
+    return jsonify(status="healthy"), 200
+
+
+if __name__ == "__main__":
+    app.logger.info("Starting Flask application")
+
+    app.run(
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", "5000")),
+    )
